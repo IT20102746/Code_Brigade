b72f5b6f13acd56d285a1d358111142c741d3d08
IT20102746 <100916629+IT20102746@users.noreply.github.com>
2022-05-08 12:26:31 +0530

    Add files via upload

---
+++ Trend1/app/src/main/java/com/example/trend1/Homepage.java
@@ -0,0 +1,35 @@
+package com.example.trend1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+
+
+
+    public void feedbak(View view) {
+
+    }
+
+    public void Profilepage(View view) {
+
+    }
+
+    public void GetItems(View view) {
+
+    }
+}
